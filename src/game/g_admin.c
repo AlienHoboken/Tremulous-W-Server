1728355088b33e535df6039b17aeb29027736eb3
Opey <Opey@users.noreply.github.com>
2013-12-05 16:33:37 -0500

    Update g_admin.c

--- src/game/g_admin.c
+++ src/game/g_admin.c
@@ -7186,7 +7186,7 @@ qboolean G_admin_report_delete( gentity_t *ent, int skiparg )
                 return qfalse;
         }
         ADMP("^3!report: ^7report for %s deleted",
-        			g_admin_reports[ i ]->reporter	);
+        			g_admin_reports[ rnum ]->reporter	);
         g_admin_reports[ rnum -1 ]->deleted = qtrue;
         G_admin_report_save();
         return qtrue;
