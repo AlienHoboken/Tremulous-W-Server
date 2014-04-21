098356118252877274626737bbbacbc20e56bc09
Jeremiah <nobody@not.real>
2014-04-20 23:22:23 -0400

    More compatibility changes

--- src/qcommon/cJSON.c
+++ src/qcommon/cJSON.c
@@ -53,13 +53,13 @@ static char* cJSON_strdup(const char* str)
 void cJSON_InitHooks(cJSON_Hooks* hooks)
 {
     if (!hooks) { /* Reset hooks */
-        cJSON_malloc = malloc;
-        cJSON_free = free;
+        cJSON_malloc = G_Alloc;
+        cJSON_free = G_Free;
         return;
     }
 
-	cJSON_malloc = (hooks->malloc_fn)?hooks->malloc_fn:malloc;
-	cJSON_free	 = (hooks->free_fn)?hooks->free_fn:free;
+	cJSON_malloc = (hooks->malloc_fn)?hooks->malloc_fn:G_Alloc;
+	cJSON_free	 = (hooks->free_fn)?hooks->free_fn:G_Free;
 }
 
 /* Internal constructor. */
